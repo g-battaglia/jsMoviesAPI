6f64fe3b5c9bd5b8ba90e69bed545552301e7692
g-battaglia <battaglia.giacomo@icloud.com>
2021-05-12 02:12:26 +0200

    Sync workaround

--- main.js
+++ main.js
@@ -1,28 +1,57 @@
 const searchInput = document.querySelector('.movieName');
 const searchBtn = document.querySelector('.searchBtn');
-const cardTitles = document.querySelectorAll('.card-title')
-const omdbURL = 'https://www.omdbapi.com/?apikey=2f09956a&t='
+const cardTitles = document.querySelectorAll('.card-title');
+const cardsContainer = document.getElementById('cardsContainer');
 
-var oReq = new XMLHttpRequest();
-
-
-function jsonMovie() {
-  searchBtn.addEventListener('click', () => {
-    movieName = searchInput.value;
-    oReq.open("GET", omdbURL + movieName);
-    oReq.send();
-    // Register a callback that will be invoked when the response arrives
-    oReq.onload = () => {
-      if (oReq.status === 200) {
-        parsedObj = JSON.parse(oReq.response);
-        return parsedObj
-      } else {
-          versionCallback(oReq.statusText, null);
-      }
-    };
-  })
+getMovieData = () => {
+  let movieName = searchInput.value;
+  let oReq = new XMLHttpRequest();
+  const omdbURL = 'https://www.omdbapi.com/?apikey=2f09956a&t='
+  oReq.open("GET", omdbURL + movieName, false);
+  oReq.send(null);
+  let jsonResp;
+  // Register a callback that will be invoked when the response arrives
+  oReq.onload = () => {
+    if (oReq.status === 200) {
+      parsedObj = JSON.parse(oReq.response);
+      // console.log(parsedObj)
+      return parsedObj
+    } else {
+        versionCallback(oReq.statusText, null);
+    }
+  };
+  parsedObj = JSON.parse(oReq.responseText);
+  return parsedObj
 }
 
-jsonObj = jsonMovie();
+getMovieData();
+
+appendMovie = () => {
+  let movieData = getMovieData();
+  console.log(movieData);
+  let movieDiv  = document.createElement('div')
+  movieDiv.classList.add('card', 'bg-light', 'shadow')
+
+  movieDiv.innerHTML = `
+  <div class="card-img-top"
+  style="background-image: url('${movieData.Poster}')">
+  </div>
+  <div class="card-body">
+  <h5 class="card-title">${movieData.Title}</h5>
+  <p class="card-text">
+  ${movieData.Plot}
+  </p>
+  <p class="card-text">
+  <small>IMDB Rating: ${movieData.Ratings[0]['Value']}</small>
+  </p>
+  <a class="btn btn-outline-dark" href="#">Go somewhere</a>
+  </div>
+  `
+  cardsContainer.prepend(movieDiv)
+
+}
+
+searchBtn.addEventListener('click', appendMovie);
+
 
 
